6a4aa92049c49b29e31203c3c32c1c8fd7883188
suleysl <97368201+suleysl@users.noreply.github.com>
2022-04-11 17:30:37 +0100

    day38: Inheritance - method overriding

--- src/day38_Inheritance/CarTask/Tesla.java
+++ src/day38_Inheritance/CarTask/Tesla.java
@@ -3,8 +3,8 @@ package day38_Inheritance.CarTask;
 public class Tesla extends Car {
 
 
-    public Tesla(String brand, String model, int year, double price, String colour, double miles) {
-        super(brand, model, year, price, colour, miles);
+    public Tesla(String model, int year, double price, String colour, double miles) {
+        super("Tesla", model, year, price, colour, miles);
     }
 
     public void electricCar(){

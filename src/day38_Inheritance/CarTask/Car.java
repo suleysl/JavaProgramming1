017312f6a9a220fa3d29c10ff691facf9c4fa654
suleysl <97368201+suleysl@users.noreply.github.com>
2022-04-11 17:04:13 +0100

    day38: Inheritance - method overriding

--- src/day38_Inheritance/CarTask/Car.java
+++ src/day38_Inheritance/CarTask/Car.java
@@ -2,11 +2,41 @@ package day38_Inheritance.CarTask;
 
 public class Car {
 
+    public String brand, model;
+    public int year;
+    public double price;
+    public String colour;
+    public double miles;
+
+    public Car(String brand, String model, int year, double price, String colour, double miles) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.price = price;
+        this.colour = colour;
+        this.miles = miles;
 
 
+    }
 
+    public void start(){
+        System.out.println(brand + " " + model + " is starting");
+    }
 
+    public void drive(){
+        System.out.println(brand + " " + model + " is driving");
+    }
 
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", year=" + year +
+                ", price=" + price +
+                ", colour='" + colour + '\'' +
+                ", miles=" + miles +
+                '}';
+    }
 
 
 }

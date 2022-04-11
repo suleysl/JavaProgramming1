3241f60c7af33f61d297006a3f64965629ba6ee0
suleysl <97368201+suleysl@users.noreply.github.com>
2022-04-11 17:35:16 +0100

    day38: Inheritance - method overriding

---
+++ src/day38_Inheritance/CarTask/ParkingLot.java
@@ -0,0 +1,13 @@
+package day38_Inheritance.CarTask;
+
+public class ParkingLot {
+
+    public static void main(String[] args) {
+
+        Toyota toyota = new Toyota("Camry", 2020, 20000, "Gray", 123456);
+    }
+
+    Tesla tesla = new Tesla("Model S", 2022, 94000, "black", 34545);
+}
+
+

fe0d42147cfd9ebbfa7d692b538e92f2ea4973c9
suleysl <97368201+suleysl@users.noreply.github.com>
2022-04-14 16:57:26 +0100

    day40 : Final Keyword and protected Access Modifier

---
+++ src/day40_FinalKeyword/ProtectedAccessModifier.java
@@ -0,0 +1,8 @@
+package day40_FinalKeyword;
+
+public class ProtectedAccessModifier {
+
+    static String name1  = "cydeo";  //default is not visible outside the package
+
+    protected static String name2 = "Wooden spoon"; // it could be visible outside the package if the class is sub-class(child class).
+}

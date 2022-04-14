74e611118191ffb0e5f422a8ebeb771b1d373596
suleysl <97368201+suleysl@users.noreply.github.com>
2022-04-14 16:40:02 +0100

    day40 : Final Keyword

---
+++ src/day40_FinalKeyword/EncapsulationReview.java
@@ -0,0 +1,17 @@
+package day40_FinalKeyword;
+
+import day39_Recap.shapeTask.Circle;
+
+public class EncapsulationReview {
+
+
+
+    private Circle circle;
+    public Circle getCircle(){
+        return circle;
+    }
+
+    public void setCircle(Circle circle){
+       this.circle = circle;
+    }
+}

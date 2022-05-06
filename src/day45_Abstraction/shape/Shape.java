c2f62f46a15a984a576e5a4c8f33350180ff6aae
suleysl <97368201+suleysl@users.noreply.github.com>
2022-05-06 12:46:38 +0100

    day45 - interface continue

---
+++ src/day45_Abstraction/shape/Shape.java
@@ -0,0 +1,26 @@
+package day45_Abstraction.shape;
+
+public abstract class Shape {
+
+    private final String name;
+
+    public Shape(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public abstract double area();
+
+    public abstract double perimeter();
+
+    public String toString() {
+        return
+                "name='" + name + '\'' +
+                        ", area='" + area() + '\'' +
+                        ", perimeter='" + perimeter() + '\'';
+
+    }
+}
